7670d988f942a319fbf962d0f1ebfe09b3fb75b7
Rolf-Arne Strandjord <83413670+Rolf85no@users.noreply.github.com>
2021-11-16 10:50:13 +0100

    Add files via upload
    Utdanningsrad virker og skriver selv om jeg legger til ny rad. Det som gjenstår er å kunne gjenbruke funksjoner i addLine på work og kurs.

--- static/main.js
+++ static/main.js
@@ -1,11 +1,11 @@
 
-
-var eventList = ["keyup", "onchange", "click"];
+var eventList = ["keyup", "click"];
 var personalia_trigg = document.getElementById("personalia");
-
+var komp_trigg = document.getElementById("kompetanseRows");
 
 for (Event of eventList){
     personalia_trigg.addEventListener(Event, copyTextPers);
+    komp_trigg.addEventListener(Event, copyTextKomp);
 }
 
 function copyTextPers(){
@@ -18,89 +18,139 @@ function copyTextPers(){
     var fodt = document.getElementById("fodt").value;
     var kompetanse = document.getElementById("kompetanse").value;
 
-    var personalia = {
-        navn: navn,
-        adresse: adresse,
-        email: email,
-        mobil: mobil,
-        sivilstand: sivilstand.options[x].text,
-        fodt: fodt,
-        kompetanse: kompetanse
-    };
-    if(personalia){
-        localStorage.setItem("personalia", JSON.stringify(personalia)); 
-    }
-       
-    
+    var cvNavn = document.getElementById("cvNavn");
+    var cvAdresse = document.getElementById("cvAdresse");
+    var cvEmail = document.getElementById("cvEmail")
+    var cvMobil = document.getElementById("cvMobil")
+    var cvSivilstand = document.getElementById("cvSivilstand");
+    var cvFodt = document.getElementById("cvFodt")
+    var cvKompetanse = document.getElementById("cvKompetanse")
+
+    cvNavn.value = navn;
+    cvAdresse.value = adresse;
+    cvEmail.value = email;
+    cvMobil.value = mobil;
+    cvSivilstand.value = sivilstand.options[x].text;
+    cvFodt.value = fodt;
+    cvKompetanse.value = kompetanse;
 }
 
-let i = 1;
-let utArrCounter = 0;
-var utArray = [];
-
-
-let myArray = []
-function copyTextKomp(counters, yEd, uni, spec){
+function copyTextKomp(){
     var yearEd = document.getElementById("yearEd").value;
     var university = document.getElementById("university").value.trim();
     var speciality = document.getElementById("speciality").value.trim();
-    myArray[0] = yearEd;
-    myArray[1] = university;
-    myArray[2] = speciality;
-    if (counters > 0){
-        myArray.push(yEd);
-        myArray.push(uni);
-        myArray.push(spec);
-    }
-    localStorage.setItem("myArray", JSON.stringify(myArray));
+
+    var cvYearEd = document.getElementById("cvYearEd");
+    var cvUniversity = document.getElementById("cvUniversity");
+    var cvSpeciality = document.getElementById("cvSpeciality");
+
+    
+
+    if (i > 1){
+        cvArray[i-1].querySelector(`#cvYe${i-1}`).value = utArray[i-1].querySelector(`#yearEd${i-1}`).value;
+        cvArray[i-1].querySelector(`#cvUni${i-1}`).value = utArray[i-1].querySelector(`#university${i-1}`).value;
+        cvArray[i-1].querySelector(`#cvSpec${i-1}`).value = utArray[i-1].querySelector(`#speciality${i-1}`).value;  
+    } 
+
+    else{
+        cvYearEd.value = yearEd;
+        cvUniversity.value = university;
+        cvSpeciality.value = speciality;
+    }      
 
 }
+var i = 1;
+var utArray = [];
+var cvArray = [];
 
-function addLine(){
+function addLine(firstRow, cvFirstrow, whichLine){
     if (i <= 1){  
-        // Velger utdanning_row og kopierer den, fjerne alle verdier, 
-        // Setter neste row id til utdanning_row i.
-        var edrow = document.getElementById('utdanning_row');
-        utArray[0] = edrow;
-        var newRow = utArray[0].cloneNode(true);
-        newRow.querySelector("#yearEd").value = '';
-        newRow.querySelector("#university").value = '';
-        newRow.querySelector("#speciality").value = '';
-        newRow.setAttribute('id',`utdanning_row${i}`);
+        // Lage en array til hver row
+        utArray[0] = firstRow;
+        cvArray[0] = cvFirstrow;
 
+        // Kloner hver row
+        var newRow = utArray[0].cloneNode(true);
+        var cvNewRow = cvArray[0].cloneNode(true);
+        var x,y;
+        // Tømmer den nye raden for tekst
+
+        x =newRow.querySelectorAll('input');
+        z =cvNewRow.querySelectorAll('input');
+
+        for(y = 0; y < x.length; y++){
+            x[y].value = '';
+            z[y].value = '';
+        }
+
+        if (whichLine === 0){
+            // Setter ny id på raden
+        newRow.setAttribute('id',`utdanning_row${i}`);
+        cvNewRow.setAttribute('id',`cvUtdanning_row${i}`);
+
+        // Form rename id 
         newYearEd  = newRow.querySelector("#yearEd");
         newYearEd.id = `yearEd${i}`;
         newUni  = newRow.querySelector("#university");
         newUni.id = `university${i}`;
         newSpec  = newRow.querySelector("#speciality");
         newSpec.id = `speciality${i}`;
+
+        // Cv rename id
+        cvNewYearEd  = cvNewRow.querySelector("#cvYearEd");
+        cvNewYearEd.id = `cvYe${i}`;
+        cvNewUni  = cvNewRow.querySelector("#cvUniversity");
+        cvNewUni.id = `cvUni${i}`;
+        cvNewSpec  = cvNewRow.querySelector("#cvSpeciality");
+        cvNewSpec.id = `cvSpec${i}`; 
+
+        }    
+
         utArray.push(newRow);
+        cvArray.push(cvNewRow);
         utArray[0].parentNode.insertBefore(utArray[1], utArray[0].nextSibling);
-        utArrCounter++;
+        cvArray[0].parentNode.insertBefore(cvArray[1], cvArray[0].nextSibling);
     }
 
-    else{
-        copyTextKomp(utArrCounter,utArray[utArrCounter].querySelector("input[type='month']").value, 
-        utArray[utArrCounter].querySelector("input[type='text']").value, 
-        utArray[utArrCounter].querySelector(`#speciality${utArrCounter}`).value);
-
+    else{ 
         utArray[i] = utArray[i-1].cloneNode(true);
-        utArray[i].querySelector(`#yearEd${i-1}`).value = '';
-        utArray[i].querySelector(`#university${i-1}`).value = '';
-        utArray[i].querySelector(`#speciality${i-1}`).value = '';
-        utArray[i].setAttribute('id',`utdanning_row${i}`);
+        cvArray[i] = cvArray[i-1].cloneNode(true);
+        
+        x =utArray[i].querySelectorAll('input');
+        z =cvArray[i].querySelectorAll('input');
 
-        newYearEd  = utArray[i].querySelector(`#yearEd${i-1}`);
-        newYearEd.id = `yearEd${i}`;
-        newUni  = utArray[i].querySelector(`#university${i-1}`);
-        newUni.id = `university${i}`;
-        newSpec = utArray[i].querySelector(`#speciality${i-1}`);
-        newSpec.id = `speciality${i}`;
+        for(y = 0; y < x.length; y++){
+            x[y].value = '';
+            z[y].value = '';
+        }
+
+        if (whichLine === 0){
+            utArray[i].setAttribute('id',`utdanning_row${i}`);
+            cvArray[i].setAttribute('id',`cvUtdanning_row${i}`);
+    
+            // Rename form siden jeg har duplisert den forrige raden har hver column samme navn som forrige
+            newYearEd  = utArray[i].querySelector(`#yearEd${i-1}`);
+            newYearEd.id = `yearEd${i}`;
+            newUni  = utArray[i].querySelector(`#university${i-1}`);
+            newUni.id = `university${i}`;
+            newSpec = utArray[i].querySelector(`#speciality${i-1}`);
+            newSpec.id = `speciality${i}`;
+    
+            // Rename CV
+            cvNewYearEd = cvArray[i].querySelector(`#cvYe${i-1}`);
+            cvNewYearEd.id = `cvYe${i}`;
+            cvNewUni = cvArray[i].querySelector(`#cvUni${i-1}`);
+            cvNewUni.id = `cvUni${i}`;
+            cvNewSpec = cvArray[i].querySelector(`#cvSpec${i-1}`);
+            cvNewSpec.id = `cvSpec${i}`;
+            
+        }
+        
+
         utArray[i-1].parentNode.insertBefore(utArray[i], utArray[i-1].nextSibling);
-        utArrCounter++;
+        cvArray[i-1].parentNode.insertBefore(cvArray[i], cvArray[i-1].nextSibling);
     } 
     i ++;
-
         
 }
 
@@ -109,60 +159,13 @@ function addLine(){
 // Funksjon for å lage stor bokstav etter space.
 function capitalize(str) {
     var splitStr = str.toLowerCase().split(' ');
-    for (let i = 0; i <splitStr.length; i++){
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    for (let j = 0; j <splitStr.length; j++){
+        splitStr[j] = splitStr[j].charAt(0).toUpperCase() + splitStr[j].substring(1);
     }
     return splitStr.join(' ');
   }
 
-// Resetter form
-  
-function reset(){
-    for(key in localStorage) {
-        delete localStorage[key];
-    }
 
-}
-
-cvArray =[]
-
-function getText(){
-    var personalia = JSON.parse(localStorage.getItem("personalia"));
-
-    if (personalia != null){
-        document.getElementById("cvNavn").innerHTML=personalia.navn;
-        document.getElementById("cvAdresse").innerHTML=personalia.adresse;
-        document.getElementById("cvEmail").innerHTML=personalia.email;
-        document.getElementById("cvMobil").innerHTML=personalia.mobil;
-        document.getElementById("cvSivilstand").innerHTML=personalia.sivilstand;
-        document.getElementById("cvFodt").innerHTML=personalia.fodt;   
-        document.getElementById("cvKompetanse").innerHTML=personalia.kompetanse;
-    }
-    
-    var kompetanseValues = JSON.parse(localStorage.getItem("myArray"));
-    if(kompetanseValues){
-        document.getElementById("cvYearEd").innerHTML=kompetanseValues[0];
-        document.getElementById("cvUniversity").innerHTML=kompetanseValues[1];
-        document.getElementById("cvSpeciality").innerHTML=kompetanseValues[2];
-    }
-    
-    if (kompetanseValues.length > 3){
-        var edrow = document.getElementById('cvutdanning_row');
-        var newRow = edrow(true);
-        newRow.querySelector("#cvYearEd").value = '';
-        newRow.querySelector("#cvUniversity").value = '';
-        newRow.querySelector("#cvSpeciality").value = '';
-        newRow.setAttribute('id',`cvutdanning_row${i}`);
-
-        newYearEd  = newRow.querySelector("#cvYearEd");
-        newYearEd.id = `cvYearEd${i}`;
-        newUni  = newRow.querySelector("#cvUniversity");
-        newUni.id = `university${i}`;
-        newSpec  = newRow.querySelector("#cvSpeciality");
-        newSpec.id = `cvSpeciality${i}`;
-        edrow.parentNode.insertBefore(newRow, edrow.nextSibling);
-    }
-}
 
 
 
